8793a8b55ae87a79ecbff33b5b16a5dd9ac5a355
Bayesian <kexiongwen@gmail.com>
2023-04-22 23:03:49 +0800

    update

--- BQR.m
+++ BQR.m
@@ -3,8 +3,6 @@ function[beta_sample]=BQR(Y,X,Q)
 M=10000;
 burn_in=10000;
 S=size(X);
-T1=1e-2;
-T2=1e-4;
 c1=(0.5*Q*(1-Q)).^0.5;
 c2=(1-2*Q)/(Q*(1-Q));
 beta_sample=zeros(S(2),M+burn_in);
@@ -31,13 +29,11 @@ for i=2:(M+burn_in)
     %Preconditioning covariance matrix
     GXTDXG=GXTD*GXTD';
 
-    Mask1=G<T1;
-
     %Sample b
     b=GXTD*DY+GXTD*randn(S(1),1)+randn(S(2),1);
 
     %Solve Preconditioning the linear system by conjugated gradient method
-    beta_tilde=cgs(sparse(GXTDXG.*(1-Mask1*Mask1')+speye(S(2))),b,1e-3);
+    beta_tilde=cgs(GXTDXG+speye(S(2)),b,1e-3);
 
     %revert to the solution of the original system
     beta_sample(:,i)=G.*beta_tilde;
@@ -49,15 +45,15 @@ for i=2:(M+burn_in)
     a_sample=1./gamrnd(1,1./(1+lam_sample));
 
     ink=lam_sample.^2.*abs(beta_sample(:,i));
-    Mask2=ink<T2;
+    
 
     % Sampling V
-    v_sample(~Mask2)=2./random('InverseGaussian',1./sqrt(ink(~Mask2)),1);
-    v_sample(Mask2)=gamrnd(0.5,4*ones(sum(Mask2),1));
+    v_sample=2./random('InverseGaussian',1./sqrt(ink),1);
+  
 
     % Sampling tau
-    tau_sample(~Mask2)=v_sample(~Mask2)./sqrt(random('InverseGaussian',v_sample(~Mask2)./ink(~Mask2),1));
-    tau_sample(Mask2)=sqrt(gamrnd(0.5,2*v_sample(Mask2).^2));
+    tau_sample=v_sample./sqrt(random('InverseGaussian',v_sample./ink,1));
+
 
     % Sampling omega
     omega_sample=random('InverseGaussian',2./abs(Y-X*beta_sample(:,i)),1)/(2*Q*(1-Q));
